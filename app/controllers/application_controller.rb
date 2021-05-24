402ff519c77db6096d62b8b578a26d8c87268c88
T-70ya <ahamo109docomo@gmail.com>
2021-05-24 16:57:31 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_ninsho
+
+  private
+
+  def basic_ninsho
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'liner' && password == '0000'
+    end
+  end
 end
