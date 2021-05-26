d5f7b0d28d0828868ac682e692ce04134b8c514a
T-70ya <ahamo109docomo@gmail.com>
2021-05-26 17:28:01 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
-  
+  expend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :haisoryo
+  belongs_to :area
+  belongs_to :day
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :haisoryo_id
+    validates :area_id
+    validates :day_id
+  end
 end
--- db/migrate/20210526053325_create_items.rb
+++ db/migrate/20210526053325_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id,     null: false
       t.integer :status_id,       null: false
       t.integer :area_id,         null: false
-      t.integer :days_id,         null: false
+      t.integer :day_id,         null: false
       t.integer :haisoryo_id,     null: false
       t.integer :price,           null: false
       t.references :user,         foreign_key: true
