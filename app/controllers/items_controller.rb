c87e3833e649de9ff1b90d5823735a6ea227da20
T-70ya <ahamo109docomo@gmail.com>
2021-05-27 18:57:08 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :edit_ng, only: :edit
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,7 +24,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-  
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(params_item)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
 
@@ -31,5 +41,12 @@ class ItemsController < ApplicationController
 
   def params_item
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :haisoryo_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def edit_ng
+    @item = Item.find(params[:id])
+    unless user_signed_in? && @item.user.id == current_user.id
+      redirect_to root_path
+    end
   end
 end
