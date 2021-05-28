0e366907f716039e663ca375c1bd92b0a8497ccf
T-70ya <ahamo109docomo@gmail.com>
2021-05-28 10:47:34 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,12 @@ class ItemsController < ApplicationController
     end
   end
 
-
-  
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
 
 
   private
