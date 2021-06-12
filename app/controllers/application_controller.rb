1720ead44243498593a4860d06e57cb6796e2872
T-70ya <ahamo109docomo@gmail.com>
2021-06-12 20:38:23 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_ninsho
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'liner' && password == '0000'
+      username == ENV["BASIC_NINSHO_USER"] && password == ENV["BASIC_NINSHO_PASSWORD"]
     end
   end
   
